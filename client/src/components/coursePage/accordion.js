5256a7a88e43d699e8ce8cca9764033fdf875f0c
Meowhib <mouhibenns@gmail.com>
2023-02-02 07:53:06 +0100

    Lessons are now picked from the react app

--- client/src/components/coursePage/accordion.js
+++ client/src/components/coursePage/accordion.js
@@ -1,26 +1,21 @@
 import React, { useState } from "react";
 
-const Accordion = ({ chapters, lessonClickHandler }) => {
-  const onLessonClick = (lessonId) => {
-    return () => {
-      lessonClickHandler(lessonId);
-    }
-  };
-
+const Accordion = ({ chapters }) => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="accordion" id="accordion">
-      {chapters.map((chapter, chapterIndex) => (
-        <div key={ chapterIndex } className="accordion-item">
-          <h2 className="accordion-header" id={`heading${chapterIndex}`}>
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${chapterIndex}`} aria-expanded="true" aria-controls={`collapse${chapterIndex}`}>
+    <div class="accordion" id="accordion">
+      {chapters.map((chapter, index) => (
+        <div class="accordion-item">
+          <h2 class="accordion-header" id={`heading${index}`}>
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${index}`} aria-expanded="true" aria-controls={`collapse${index}`}>
               {chapter.name}
             </button>
           </h2>
-          <div id={`collapse${chapterIndex}`} className="accordion-collapse collapse" aria-labelledby={`heading${chapterIndex}`} data-bs-parent="#accordion">
-            <div className="accordion-body p-0">
-              {chapter.lessons.map((lesson, lessonIndex) => (
-                <div key={ lessonIndex } className="card" onClick={onLessonClick(lesson._id)}>
-                  <div className="card-body">
+          <div id={`collapse${index}`} class="accordion-collapse collapse" aria-labelledby={`heading${index}`} data-bs-parent="#accordion">
+            <div class="accordion-body p-0">
+              {chapter.lessons.map((lesson) => (
+                <div class="card">
+                  <div class="card-body">
                     {lesson.name}
                   </div>
                 </div>
